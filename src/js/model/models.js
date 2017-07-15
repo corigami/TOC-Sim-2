fe62fadef3dd0a119c52f475556c9c039249ff51
Corey Willinger <corey.willinger@gmail.com>
2017-07-15 23:19:40 +0430

    refactor: converted model object to use prototype functions

--- src/js/model/models.js
+++ src/js/model/models.js
@@ -8,34 +8,44 @@
  * @constructor
  */
 var Model = function () {
-    var self = this;
-    self.name = "BaseModel";
+    var name;
 
-    self.getScenarios = function () {
-        //do nothing
-    };
-}
+    this.init();
+};
+/**
+ * Initializes the model.  Loads data from peristience into the passed array
+ */
+Model.prototype.init = function(){
+    this.name =  "BaseModel";
+};
 
 /**
  * Extends the model for local persistance;
  * @constructor
  */
 var LocalModel = function (array) {
-    var self = this;
-    self.name = "LocalModel";
+    this.init(array);
+};
+//link this object to the Parent class Prototype
+LocalModel.prototype = Object.create(Model.prototype);
 
+/**
+ * Initializes the model.  Loads data from peristience into the passed array
+ * @param array Location
+ */
+LocalModel.prototype.init = function(array){
+    this.name = "LocalModel"
     var i =0;
     scenarioDefinitions.forEach(function (el) {
         array[i] = new Scenario(el);
         i++;
     });
-
-    self.getScenarios = function(){
-        return;
-    };
-
-    self.getName = function () {
-        return self.name;
-    };
 };
-LocalModel.prototype = Object.create(Model.prototype);
+
+self.getScenarios = function(){
+    return;
+};
+
+self.getName = function () {
+    return self.name;
+};
